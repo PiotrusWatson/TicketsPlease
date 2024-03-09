b642cf68c0af70dd5bea0deb5b36f27cce6700b8
Jonathan <jonnyarchy@gmail.com>
2024-03-09 23:30:06 +0000

    Small Train

--- Scripts/SmallTrain.gd
+++ Scripts/SmallTrain.gd
@@ -2,6 +2,8 @@ extends AnimatedSprite2D
 
 @onready var path = $"../.."
 @onready var pathFollow = $".."
+@onready var timer = $"../../../../../Interface/Timer/Timer"
+@export var secondsUntilEnd = 0
 
 var traverseTime = 5 # Time it takes to traverse the path - Link to timer
 var t = 0 # Active time along the path
@@ -11,6 +13,8 @@ var pathLength = 0 # Length of the path
 func _ready():	
 	play("default")
 	pathLength = path.get_curve().get_baked_length()
+	
+	traverseTime = GetTime()
 	pass # Replace with function body.
 
 
@@ -21,3 +25,7 @@ func _process(delta):
 	t += delta
 	pathFollow.progress += t
 	pass
+
+func GetTime():
+	secondsUntilEnd = timer.time_left
+	return secondsUntilEnd
