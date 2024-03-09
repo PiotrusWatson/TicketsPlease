e01872cd9a6b1cd8eee1d8e82b20aee854a1d663
Jonathan <jonnyarchy@gmail.com>
2024-03-09 11:04:12 +0000

    Dunno?

--- Scripts/UpdateTimer.gd
+++ Scripts/UpdateTimer.gd
@@ -5,28 +5,37 @@ extends Label
 # 0 = Paused
 # 1 = Active
 # 2 = Reset, go to Paused
-@export var frameRate = 0.0
+var frameRate = 0.0
 @export var timerCountdownMinutes = 0
 @export var timerCountdownSeconds = 0
-@export var timerCountdownFrames = 0
+var timerCountdownFrames = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	frameRate = Performance.get_monitor(Performance.TIME_FPS)
-	print(frameRate)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if timerStatus == 1:
-		timerCountdownFrames -= 1
+		timerCountdownFrames -= delta
 		if timerCountdownFrames <= 0:
 			timerCountdownFrames = frameRate
 			timerCountdownSeconds -= 1
 		if timerCountdownSeconds <= 0:
 			timerCountdownSeconds = 59
 			timerCountdownMinutes -= 1
-		text = str(timerCountdownMinutes) + ":" + str(timerCountdownSeconds)
+		if timerCountdownSeconds < 10:
+			text = str(timerCountdownMinutes) + ":0" + str(timerCountdownSeconds)
+		else:
+			text = str(timerCountdownMinutes) + ":" + str(timerCountdownSeconds)
+	
+	if timerCountdownMinutes < 0:
+		timerCountdownMinutes = 0
+		timerCountdownSeconds = 0
+		timerStatus = 0
+		text = "Times up!"
+		#text = str(timerCountdownMinutes) + ":0" + str(timerCountdownSeconds)
 	pass
