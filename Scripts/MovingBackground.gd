8c14c7ec181f102804710b7324f3b3c3c23e510b
Jonathan <jonnyarchy@gmail.com>
2024-03-09 21:48:29 +0000

    Parallax Done Yes :)

--- Scripts/MovingBackground.gd
+++ Scripts/MovingBackground.gd
@@ -1,15 +1,12 @@
-extends ParallaxBackground
+extends ParallaxLayer
 
 @export var amount_to_move = 100
-@onready var camera = $".."
-var startingPosition
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	startingPosition = transform
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	offset.x -= amount_to_move * delta
+	motion_offset.x -= amount_to_move * delta
