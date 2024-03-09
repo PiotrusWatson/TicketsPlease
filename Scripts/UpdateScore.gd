cfcaabb3286868dc6ba801e95e3e28a811f7936b
Jonathan <jonnyarchy@gmail.com>
2024-03-09 23:16:46 +0000

    TicketCheck Setup

--- Scripts/UpdateScore.gd
+++ Scripts/UpdateScore.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
 @onready var ticketCounter = $TicketCounter
+@onready var ui = $PlayerCamera/CanvasLayer/Interface
 
 @export var ticketsChecked = 0
 @export var ticketsToCheck = 5
+var passengers : Array[Node]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	passengers = get_tree().get_nodes_in_group("passenger")
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,3 +19,6 @@ func _process(_delta):
 func ScoreUpdate():
 	ticketsChecked += 1
 	ticketCounter.text = str(ticketsChecked) + "/" + str(ticketsToCheck)
+	if ticketsChecked >= (passengers.size() * 0.75):
+		# do the win thing
+		pass
