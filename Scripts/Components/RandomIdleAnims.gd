d8a44297cdd169a43ce77072d13179931ed76fb0
piotrus <piotruswatson@gmail.com>
2024-03-09 10:57:09 +0000

    added simple npc with random idle animations

---
+++ Scripts/Components/RandomIdleAnims.gd
@@ -0,0 +1,12 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var picked_animation = sprite_frames.animations.pick_random()
+	play(picked_animation.name)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
