a9cd0df0eadff1510e6b1620d71a2172cd6bdf74
Jonathan <jonnyarchy@gmail.com>
2024-03-09 13:35:18 +0000

    Bunch o' Stuff

--- Scenes/SmallTrain.gd
+++ Scenes/SmallTrain.gd
@@ -1,11 +1,23 @@
 extends AnimatedSprite2D
 
+@onready var path = $"../.."
+@onready var pathFollow = $".."
+
+var traverseTime = 5 # Time it takes to traverse the path - Link to timer
+var t = 0 # Active time along the path
+var pathLength = 0 # Length of the path
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready():	
+	play("default")
+	pathLength = path.get_curve().get_baked_length()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if (t > traverseTime):
+		t = 0
+	t += delta
+	pathFollow.progress += t
 	pass
