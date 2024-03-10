7f9d89bce61aef251654d882d14b3d5b952aea4d
Jonathan <jonnyarchy@gmail.com>
2024-03-10 14:45:34 +0000

    Oops :)

--- Scripts/train_head.gd
+++ Scripts/train_head.gd
@@ -3,9 +3,9 @@ extends CharacterBody2D
 @onready var animatedSprite : AnimatedSprite2D = $Image
 
 #Explosions
-@onready var explosion1 : AnimatedSprite2D = $Explosion
-@onready var explosion2 : AnimatedSprite2D = $Explosion2
-@onready var explosion3 : AnimatedSprite2D = $Explosion3
+@onready var explosion1 : Node2D = $Explosion
+@onready var explosion2 : Node2D = $Explosion2
+@onready var explosion3 : Node2D = $Explosion3
 func ready():
 	animatedSprite.play("default")
 
