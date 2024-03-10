def1cf1ccb6d6e80851d792b43b4d40f8800b7e6
Jonathan <jonnyarchy@gmail.com>
2024-03-10 01:26:44 +0000

    Hehe more fireys :)

--- Scripts/Train.gd
+++ Scripts/Train.gd
@@ -9,7 +9,8 @@ var carriages: Array[PhysicsBody2D]
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	place_carriages()
-	carriages[9].MakeFire()
+	for i in range(carriage_number - 1):
+		carriages[i].MakeFire()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
